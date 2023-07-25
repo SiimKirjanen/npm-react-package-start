832157c2c4ce18f5c07af006d8174fc07f869afd
Siim <siim.kirjanen@gmail.com>
2023-07-25 16:02:14 +0300

    update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import sass from 'rollup-plugin-sass'
 import typescript from 'rollup-plugin-typescript2'
 
-import pkg from './package.json'
+import pkg from './package.json' assert {type: "json"}
 
 export default {
   input: 'src/index.tsx',
